c964a69612c052a7359c376149de956dcdc8318e
Oriol Bover Vila <bover73@gmail.com>
2024-08-08 11:36:02 +0200

    add examples cdn and npm

---
+++ examples/npm/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Token Example App</h1>
+      <p>This app is using design tokens from an npm package.</p>
+      <button className="button-primary">Primary Button</button>
+      <button className="button-secondary">Secondary Button</button>
+    </div>
+  )
+}
+
+export default App
